ec0e3dcb30bb474da38eead4abb9ace7c04e4f6a
Jimbanan <uservice371@mail.ru>
2022-03-23 21:25:25 +0300

    Edit index.html and Create registration.js

---
+++ Authorization/registration.js
@@ -0,0 +1,4 @@
+document.querySelector('#signup_sumbit').onclick = function (event) {
+    event.preventDefault();
+    window.location.href = "http://localhost:63342/Project/Registration%20employee/index.html?_ijt=jgivq0kh7kq3hq0ccvvlh7kpfu&_ij_reload=RELOAD_ON_SAVE"
+}
