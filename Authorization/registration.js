71fc78afe314978438e8fc7a2481f87ccd273518
Jimbanan <uservice371@mail.ru>
2022-05-05 10:51:06 +0300

    Added settings page and events page

--- Authorization/registration.js
+++ Authorization/registration.js
@@ -1,6 +1,6 @@
 document.querySelector('#signup_sumbit').onclick = function (event) {
     event.preventDefault();
-    window.location.href = "http://localhost:63342/Project/Registration%20employee/index.html?_ijt=jgivq0kh7kq3hq0ccvvlh7kpfu&_ij_reload=RELOAD_ON_SAVE"
+    window.location.href = "http://localhost:63342/Project/Registration%20employee/index.html?_ijt=6q7t8d93rqln3fsjb1abf4qllp&_ij_reload=RELOAD_ON_SAVE"
 }
 
 document.querySelector('#login-send').onclick = function (event) {
